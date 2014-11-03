f7cb750560f07ca5590856806462b06ac5338faa
asger@khanna.dk <asger@khanna.dk>
2014-11-03 13:57:16 +0100

    flytning af filer

--- App_Code/DBcon.cs
+++ App_Code/DBcon.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.SqlClient;
 
 /// <summary>
 /// Summary description for DBcon
 // Dette er en klasse til at oprette forbindelse til en SQL server
 public class DBcon
 {
-	public DBcon()
+    public SqlConnection SQLCON = new SqlConnection("Provider=SQLOLEDB.1;Persist Security Info=False;User ID=fdfk7_dk;Data Source=mssql3.unoeuro.com");
+    public SqlCommand Sqlcmd;
+
+    public dbmetode SqlConnection()
 	{
-		//
-		// TODO: Add constructor logic here
-		//
+        
+
+
+
+
 	}
 }
