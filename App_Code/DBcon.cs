1bf0a64fb963a4151a541b45f203a7b5831d7532
asger@khanna.dk <asger@khanna.dk>
2014-11-03 10:43:25 +0100

    Test
    Test TEst Asger

---
+++ App_Code/DBcon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for DBcon
+/// </summary>
+public class DBcon
+{
+	public DBcon()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
