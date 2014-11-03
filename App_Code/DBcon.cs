a088f3bce6379ec2a08cab4462517572f877e23d
asger@khanna.dk <asger@khanna.dk>
2014-11-03 11:26:55 +0100

    Tilføjet en komentar

--- App_Code/DBcon.cs
+++ App_Code/DBcon.cs
@@ -5,7 +5,7 @@ using System.Web;
 
 /// <summary>
 /// Summary description for DBcon
-/// </summary>
+// Dette er en klasse til at oprette forbindelse til en SQL server
 public class DBcon
 {
 	public DBcon()
