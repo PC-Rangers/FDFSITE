9332df472987c01de37edec41dbee607bc8de256
asger@khanna.dk <asger@khanna.dk>
2014-11-04 08:41:53 +0100

    Småændringer

--- App_Code/DBcon.cs
+++ App_Code/DBcon.cs
@@ -4,15 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Data.SqlClient;
 
-/// <summary>
-/// Summary description for DBcon
 // Dette er en klasse til at oprette forbindelse til en SQL server
+
 public class DBcon
 {
-    public SqlConnection SQLCON = new SqlConnection("Provider=SQLOLEDB.1;Persist Security Info=False;User ID=fdfk7_dk;Data Source=mssql3.unoeuro.com");
+    public SqlConnection sqlcon = new SqlConnection("Provider=SQLOLEDB.1;Persist Security Info=False;User ID=fdfk7_dk;Data Source=mssql3.unoeuro.com");
     public SqlCommand Sqlcmd;
 
-    public dbmetode SqlConnection()
+    public SqlConnection dbmetode()
 	{
         
 
