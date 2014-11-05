b744a4e62e930d5caf4c3902c6b6545031553930
asger@khanna.dk <asger@khanna.dk>
2014-11-05 13:10:23 +0100

    Ignore

--- FDF/FDF/Default.aspx.cs
+++ FDF/FDF/Default.aspx.cs
@@ -17,11 +17,4 @@ namespace FDF {
             //}
         }
     }
-//=======
-	public partial class Default : System.Web.UI.Page {
-		public void button1Clicked( object sender, EventArgs args ) {
-			//FIXME button1.Text = "You clicked me";
-		}
-	}
-//>>>>>>> origin/master
 }
