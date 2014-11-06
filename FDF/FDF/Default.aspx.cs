dcb7d97a354e5a05b45244ff7b67f205cf925fcc
asger@khanna.dk <asger@khanna.dk>
2014-11-06 09:18:28 +0100

    Revert "Commit af hvad jeg hra"
    This reverts commit dc905319a8d841b8e8681473ccd8568d45a2eb14.

--- FDF/FDF/Default.aspx.cs
+++ FDF/FDF/Default.aspx.cs
@@ -1,23 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
-using System.Data.SqlClient;
 
 namespace FDF 
-{
-public partial class Default : System.Web.UI.Page 
+{	
+    public partial class Default : System.Web.UI.Page 
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-
-        }
         public void LoginKnap_Click( object sender, EventArgs args ) 
         {
             //DBcon DBLogIn = new DBcon();
-            //if(Ged.Text)
+            //if(FindControl("username") != "" && FindControl("password") != "")
             //{
             //    DBLogIn.SQLcmd = ("select * from users where username = '" + username.Text + "' and password = '" + password.Text + "' ");
             //    DBLogIn.ConnectionOpen();
