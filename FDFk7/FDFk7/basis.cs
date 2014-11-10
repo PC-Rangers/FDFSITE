9125ae48ccc68dacd50876245578b326e009972c
Daniel V. Sandholt <gunsnap@gmail.com>
2014-11-10 08:02:33 +0100

    try på login

--- FDFk7/FDFk7/basis.cs
+++ FDFk7/FDFk7/basis.cs
@@ -49,33 +49,37 @@ namespace FDFk7 {
 			Button btnLogin = ( ( Button )obj[ 2 ] );
 			HttpSessionState session = ( ( HttpSessionState )obj[ 3 ] );
 
-			if( fnk == "Login" ) {
-				SqlConnection Con = new SqlConnection( "Data Source=mssql3.unoeuro.com;Initial Catalog=fdfk7_dk_db;Persist Security Info=True;User ID=fdfk7_dk;Password=4Xbc8tun" );
-				SqlCommand cmd = new SqlCommand( "select * from users where username = '" + txtBruger.Text + "' and password = '" + txtAdgang.Text + "' " );
-				Con.Open();
-				cmd.Connection = Con;
-				SqlDataReader DB_Reader = cmd.ExecuteReader();
-				while( DB_Reader.Read() ) {
-					string DB_UserName = DB_Reader[ "username" ].ToString();
-					string DB_PassWord = DB_Reader[ "password" ].ToString();
-					string DB_rights = DB_Reader[ "return" ].ToString();
+			try {
+				if( fnk == "Login" ) {
+					SqlConnection Con = new SqlConnection( "Data Source=mssql3.unoeuro.com;Initial Catalog=fdfk7_dk_db;Persist Security Info=True;User ID=fdfk7_dk;Password=4Xbc8tun" );
+					SqlCommand cmd = new SqlCommand( "select * from users where username = '" + txtBruger.Text + "' and password = '" + txtAdgang.Text + "' " );
+					Con.Open();
+					cmd.Connection = Con;
+					SqlDataReader DB_Reader = cmd.ExecuteReader();
+					while( DB_Reader.Read() ) {
+						string DB_UserName = DB_Reader[ "username" ].ToString();
+						string DB_PassWord = DB_Reader[ "password" ].ToString();
+						string DB_rights = DB_Reader[ "return" ].ToString();
 
-					if( txtBruger.Text == DB_UserName && txtAdgang.Text == DB_PassWord ) {
-						if( ( DB_rights == "1" ) || ( DB_rights == "2" ) ) {
-							txtBruger.Text = "Velkommen " + DB_UserName;
-							btnLogin.Text = "Log ud fra rettighedsniveau " + DB_rights;
-							session[ "UserAuthentication" ] = DB_UserName;
+						if( txtBruger.Text == DB_UserName && txtAdgang.Text == DB_PassWord ) {
+							if( ( DB_rights == "1" ) || ( DB_rights == "2" ) ) {
+								txtBruger.Text = "Velkommen " + DB_UserName;
+								btnLogin.Text = "Log ud fra rettighedsniveau " + DB_rights;
+								session[ "UserAuthentication" ] = DB_UserName;
+							}
+							txtBruger.Visible = false;
+							txtAdgang.Visible = false;
 						}
-						txtBruger.Visible = false;
-						txtAdgang.Visible = false;
 					}
+				} else {
+					txtBruger.Text = "";
+					txtBruger.Visible = true;
+					txtAdgang.Visible = true;
+					btnLogin.Text = "Log ind";
+					session.Abandon();
 				}
-			} else {
-				txtBruger.Text = "";
-				txtBruger.Visible = true;
-				txtAdgang.Visible = true;
-				btnLogin.Text = "Log ind";
-				session.Abandon();
+			} catch( Exception e ) {
+
 			}
 		}
 
