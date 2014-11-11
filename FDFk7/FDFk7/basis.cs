acd6c765b2d7a9a959172dc64eb4c3e0e173cf05
Asger Khanna <Asger Khanna>
2014-11-11 10:02:09 +0100

    TEst af om der er skrevet noget i brugernavn og kode

--- FDFk7/FDFk7/basis.cs
+++ FDFk7/FDFk7/basis.cs
@@ -8,14 +8,14 @@ using System.Web.UI.WebControls;
 namespace FDFk7 {
 	public class Basis {
 
-		private string mainURL = "http://fdfk7.dk.nt8.unoeuro-server.com/";
+        private string mainURL = "http://localhost:51017/";
+            //"http://fdfk7.dk.nt8.unoeuro-server.com/";
 		//"http://12.0.0.1:8080/";
 		//http://127.0.0.1:51017/
 		private HttpResponse hR;
 		private HttpSessionState session;
 
 		public Basis( string fnk, object[] obj ) {
-			mainURL = "http://fdfk7.dk.nt8.unoeuro-server.com/";
 			switch( fnk ) {
 				case "Load":
 					session = ( HttpSessionState )obj[ 3 ];
@@ -56,34 +56,43 @@ namespace FDFk7 {
 			Button btnLogin = ( ( Button )obj[ 2 ] );
 
 			try {
-				if( fnk == "Login" ) {
-					SqlConnection Con = new SqlConnection( "Data Source=mssql3.unoeuro.com;Initial Catalog=fdfk7_dk_db;Persist Security Info=True;User ID=fdfk7_dk;Password=4Xbc8tun" );
-					SqlCommand cmd = new SqlCommand( "select brugernavn, kodeord, retur from users where brugernavn = '" + txtBruger.Text + "' and kodeord = '" + txtAdgang.Text + "' " );
-					Con.Open();
-					cmd.Connection = Con;
-					SqlDataReader DB_Reader = cmd.ExecuteReader();
-					while( DB_Reader.Read() ) {
-						string DB_UserName = DB_Reader[ "brugernavn" ].ToString();
-						string DB_PassWord = DB_Reader[ "kodeord" ].ToString();
-						string DB_rights = DB_Reader[ "retur" ].ToString();
+                if (fnk == "Login")
+                {
+                    if (txtBruger.Text != "" && txtAdgang.Text != "")
+                    {
+                        SqlConnection Con = new SqlConnection("Data Source=mssql3.unoeuro.com;Initial Catalog=fdfk7_dk_db;Persist Security Info=True;User ID=fdfk7_dk;Password=4Xbc8tun");
+                        SqlCommand cmd = new SqlCommand("select brugernavn, kodeord, retur from users where brugernavn = '" + txtBruger.Text + "' and kodeord = '" + txtAdgang.Text + "' ");
+                        Con.Open();
+                        cmd.Connection = Con;
+                        SqlDataReader DB_Reader = cmd.ExecuteReader();
+                        while (DB_Reader.Read())
+                        {
+                            string DB_UserName = DB_Reader["brugernavn"].ToString();
+                            string DB_PassWord = DB_Reader["kodeord"].ToString();
+                            string DB_rights = DB_Reader["retur"].ToString();
 
-						if( txtBruger.Text == DB_UserName && txtAdgang.Text == DB_PassWord ) {
-							if( ( DB_rights == "1" ) || ( DB_rights == "2" ) ) {
-								txtBruger.Text = "Velkommen " + DB_UserName;
-								btnLogin.Text = "Log ud fra rettighedsniveau " + DB_rights;
-								session[ "UserAuthentication" ] = DB_UserName;
-							}
-							txtBruger.Visible = false;
-							txtAdgang.Visible = false;
-						}
-					}
-				} else {
-					txtBruger.Text = "";
-					txtBruger.Visible = true;
-					txtAdgang.Visible = true;
-					btnLogin.Text = "Log ind";
-					session.Abandon();
-				}
+                            if (txtBruger.Text == DB_UserName && txtAdgang.Text == DB_PassWord)
+                            {
+                                if ((DB_rights == "1") || (DB_rights == "2"))
+                                {
+                                    txtBruger.Text = "Velkommen " + DB_UserName;
+                                    btnLogin.Text = "Log ud fra rettighedsniveau " + DB_rights;
+                                    session["UserAuthentication"] = DB_UserName;
+                                }
+                                txtBruger.Visible = false;
+                                txtAdgang.Visible = false;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        txtBruger.Text = "";
+                        txtBruger.Visible = true;
+                        txtAdgang.Visible = true;
+                        btnLogin.Text = "Log ind";
+                        session.Abandon();
+                    }
+                }
 			} catch( Exception e ) {
 
 			}
