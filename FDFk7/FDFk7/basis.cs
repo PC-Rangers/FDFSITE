f7c1d8031d32c830e4a7d53c1acc238ef73981a9
Asger Khanna <Asger Khanna>
2014-11-11 09:34:06 +0100

    Opdatering af login

--- FDFk7/FDFk7/basis.cs
+++ FDFk7/FDFk7/basis.cs
@@ -58,14 +58,14 @@ namespace FDFk7 {
 			try {
 				if( fnk == "Login" ) {
 					SqlConnection Con = new SqlConnection( "Data Source=mssql3.unoeuro.com;Initial Catalog=fdfk7_dk_db;Persist Security Info=True;User ID=fdfk7_dk;Password=4Xbc8tun" );
-					SqlCommand cmd = new SqlCommand( "select * from users where username = '" + txtBruger.Text + "' and password = '" + txtAdgang.Text + "' " );
+					SqlCommand cmd = new SqlCommand( "select brugernavn, kodeord, retur from users where brugernavn = '" + txtBruger.Text + "' and kodeord = '" + txtAdgang.Text + "' " );
 					Con.Open();
 					cmd.Connection = Con;
 					SqlDataReader DB_Reader = cmd.ExecuteReader();
 					while( DB_Reader.Read() ) {
-						string DB_UserName = DB_Reader[ "username" ].ToString();
-						string DB_PassWord = DB_Reader[ "password" ].ToString();
-						string DB_rights = DB_Reader[ "return" ].ToString();
+						string DB_UserName = DB_Reader[ "brugernavn" ].ToString();
+						string DB_PassWord = DB_Reader[ "kodeord" ].ToString();
+						string DB_rights = DB_Reader[ "retur" ].ToString();
 
 						if( txtBruger.Text == DB_UserName && txtAdgang.Text == DB_PassWord ) {
 							if( ( DB_rights == "1" ) || ( DB_rights == "2" ) ) {
