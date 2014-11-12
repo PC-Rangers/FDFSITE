11e35222c37c656ecd291848668d71035562148f
Daniel V. Sandholt <gunsnap@gmail.com>
2014-11-12 12:53:20 +0100

    Det virker.. :·)

--- FDFk7/FDFk7/Basis.cs
+++ FDFk7/FDFk7/Basis.cs
@@ -6,50 +6,53 @@ using System.Web.UI.WebControls;
 using System.Text;
 using System.Security.Cryptography;
 
-namespace FDFk7 {
-	public class Basis {
+namespace FDFk7
+{
+    public class Basis
+    {
 
-		private string mainURL = "http://localhost:51017/";
-		//"http://fdfk7.dk.nt8.unoeuro-server.com/";
-		//"http://127.0.0.1:8080/";
-		//http://127.0.0.1:51017/
-		private HttpResponse hR;
-		private HttpSessionState session;
+        private string mainURL = "http://127.0.0.1:8080/";
+        /*Mac: "http://127.0.0.1:8080/";
+        * Win: "http://localhost:51017/";
+        * Web: "http://fdfk7.dk.nt8.unoeuro-server.com/";
+        */
+        private HttpResponse hR;
+        private HttpSessionState session;
 
-		/** Load og LoginOut **/
-		public Basis( string fnk, object[] obj )
-		{
-			switch( fnk )
-			{
-			case "Load":
-				session = (HttpSessionState)obj[3];
-				if( session["UserAuthentication"] != null )
-				{
-					Load( obj );
-				}
-				break;
-			case "Login":
-			case "Logout":
-				session = (HttpSessionState)obj[3];
-				LogInUd( obj );
-				break;//FIXME LogInUd skal laves om så man kan bruge GaaTil; Kig i Basis( object[] obj )
-			}
-		}
+        /** Load og LoginOut **/
+        public Basis( string fnk, object[] obj )
+        {
+            switch( fnk )
+            {
+                case "Load":
+                    session = (HttpSessionState)obj[3];
+                    if( session["UserAuthentication"] != null )
+                    {
+                        Load( obj );
+                    }
+                    break;
+                case "Login":
+                case "Logout":
+                    session = (HttpSessionState)obj[3];
+                    LogInUd( fnk, obj );
+                    break;//FIXME LogInUd skal laves om så man kan bruge GaaTil; Kig i Basis( object[] obj )
+            }
+        }
 
-		/** GaaTil **/
-		public Basis( object[] obj )
-		{
-			switch( ((Button)obj[0]).ID )
-			{
-			case "btnDefault":
-			case "btnUdlejning":
-			case "btnOmOs":
-			case "btnKontakt":
-				((HttpResponse)obj[1]).Redirect( mainURL + ((Button)obj[0]).ID.Substring( 3 ) + ".aspx" );
-				break; // Sideskift ( adresse + knapnavn + .aspx)
-				//fx = http://fdfk7.dk.nt8.unoeuro-server.com/ + Default + .aspx
+        /** GaaTil **/
+        public Basis( object[] obj )
+        {
+            switch( ((Button)obj[0]).ID )
+            {
+                case "btnDefault":
+                case "btnUdlejning":
+                case "btnOmOs":
+                case "btnKontakt":
+                    ((HttpResponse)obj[1]).Redirect( mainURL + ((Button)obj[0]).ID.Substring( 3 ) + ".aspx" );
+                    break; // Sideskift ( adresse + knapnavn + .aspx)
+            //fx = http://fdfk7.dk.nt8.unoeuro-server.com/ + Default + .aspx
 
-				/*
+            /*
                 case "btnLogin":
                 case "btnLogout":
                     session = (HttpSessionState)obj[3];
@@ -64,115 +67,116 @@ namespace FDFk7 {
                         Basis bob = new Basis("Login", new object[]{ txtBruger, txtAdgang, btnLogin, Session });
                     }
             */
-			}
-		}
+            }
+        }
 
-		private void Load( object[] obj )
-		{//FIXME skal laves så der også Loades når man ikke er logget ind
-			((TextBox)obj[0]).Visible = false;
-			((TextBox)obj[1]).Visible = false;
-			Button btn = (Button)obj[2];
-			btn.Text = "Log ud " + session["UserAuthentication"];
-		}
-
-
-
-		private void LogInUd( string fnk, object[] obj ) {
-			TextBox txtBruger = ( ( TextBox )obj[ 0 ] );
-			TextBox txtAdgang = ( ( TextBox )obj[ 1 ] );
-			Button btnLogin = ( ( Button )obj[ 2 ] );
-
-			try {
-				if (fnk == "Login")
-				{
-					// Tjek for input af brugernavn og adgangskode
-					if (txtBruger.Text != "" && txtAdgang.Text != "")
-					{
-						// Opretter forbindelse til databasen
-						SqlConnection Con = new SqlConnection( "Data Source=mssql3.unoeuro.com;Initial Catalog=fdfk7_dk_db;Persist Security Info=True;User ID=fdfk7_dk;Password=4Xbc8tun" );
+        private void Load( object[] obj )
+        {//FIXME skal laves så der også Loades når man ikke er logget ind
+            ((TextBox)obj[0]).Visible = false;
+            ((TextBox)obj[1]).Visible = false;
+            Button btn = (Button)obj[2];
+            btn.Text = "Log ud " + session["UserAuthentication"];
+        }
 
 
-						// querry der tæller brugere med det brugernavn i databasen
-						SqlCommand cmd_count = new SqlCommand("SELECT count(BrugerNavn) FROM USR_Brugere WHERE BrugerNavn = '" + txtBruger.Text + "' ", Con);
 
-							Con.Open();
-							// Tæller brugere med det brugernavn i databasen
-							int total = (Int32)cmd_count.ExecuteScalar();
+        private void LogInUd( string fnk, object[] obj )
+        {
+            TextBox txtBruger = ((TextBox)obj[0]);
+            TextBox txtAdgang = ((TextBox)obj[1]);
+            Button btnLogin = ((Button)obj[2]);
+            //string fnk = ((Button)obj[0]).ID;
+            try
+            {
+                if( fnk == "Login" )
+                {
+                    // Tjek for input af brugernavn og adgangskode
+                    if( txtBruger.Text != "" && txtAdgang.Text != "" )
+                    {
+                        // Opretter forbindelse til databasen
+                        SqlConnection Con = new SqlConnection( "Data Source=mssql3.unoeuro.com;Initial Catalog=fdfk7_dk_db;Persist Security Info=True;User ID=fdfk7_dk;Password=4Xbc8tun" );
 
-							if (total == 1)
-							{
-								// Valg af bruger fra databasen
-								SqlCommand cmd_select = new SqlCommand( "SELECT BrugerNavn,Adgangskode,Salt,Rettighedder FROM USR_Brugere WHERE BrugerNavn = '" + txtBruger.Text + "' " );
 
-								cmd_select.Connection = Con;
-								SqlDataReader DB_Reader = cmd_select.ExecuteReader();
+                        // querry der tæller brugere med det brugernavn i databasen
+                        SqlCommand cmd_count = new SqlCommand( "SELECT count(BrugerNavn) FROM USR_Brugere WHERE BrugerNavn = '" + txtBruger.Text + "' ", Con );
 
-								while (DB_Reader.Read())
-								{
-									string DB_UserName 	= DB_Reader["BrugerNavn"].ToString();
-									string DB_PassWord 	= DB_Reader["Adgangskode"].ToString();
-									string DB_Salt 		= DB_Reader["Salt"].ToString();
-									string DB_rights 	= DB_Reader["Rettighedder"].ToString();
+                        Con.Open();
+                        // Tæller brugere med det brugernavn i databasen
+                        int total = (Int32)cmd_count.ExecuteScalar();
 
-									string txtPass = txtAdgang.Text;
+                        if( total == 1 )
+                        {
+                            // Valg af bruger fra databasen
+                            SqlCommand cmd_select = new SqlCommand( "SELECT BrugerNavn,Adgangskode,Salt,Rettighedder FROM USR_Brugere WHERE BrugerNavn = '" + txtBruger.Text + "' " );
 
-									// Fastslår længden af koden
-									int length = txtPass.Length;
-									// Deler koden op i 2
-									string firstHalf = txtPass.Substring(0, length / 2);
-									string secondHalf = txtPass.Substring(length / 2, length - (length / 2));
+                            cmd_select.Connection = Con;
+                            SqlDataReader DB_Reader = cmd_select.ExecuteReader();
 
-									/*
+                            while( DB_Reader.Read() )
+                            {
+                                string DB_UserName = DB_Reader["BrugerNavn"].ToString();
+                                string DB_PassWord = DB_Reader["Adgangskode"].ToString();
+                                string DB_Salt = DB_Reader["Salt"].ToString();
+                                string DB_rights = DB_Reader["Rettighedder"].ToString();
+
+                                string txtPass = txtAdgang.Text;
+
+                                // Fastslår længden af koden
+                                int length = txtPass.Length;
+                                // Deler koden op i 2
+                                string firstHalf = txtPass.Substring( 0, length / 2 );
+                                string secondHalf = txtPass.Substring( length / 2, length - (length / 2) );
+
+                                /*
 									 * TEST behandling af krydring af password
 									txtBruger.Text = firstHalf + " " + DB_Salt + " " + secondHalf;
 									*/
 
-									// Samler password 
-								string toHash =  firstHalf + DB_Salt + secondHalf;
-								//string hashPass = System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(toHash, "sha1");
+                                // Samler password 
+                                string toHash = firstHalf + DB_Salt + secondHalf;
+                                //string hashPass = System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(toHash, "sha1");
 //
-								SHA512 sha = new SHA512Managed();
-								byte[] hash = sha.ComputeHash(Encoding.ASCII.GetBytes(toHash));
+                                SHA512 sha = new SHA512Managed();
+                                byte[] hash = sha.ComputeHash( Encoding.ASCII.GetBytes( toHash ) );
 
-								StringBuilder stringBuilder = new StringBuilder();
-								foreach (byte b in hash) {
-									stringBuilder.AppendFormat("{0:x2}", b);
-								}
-								string hashPass = stringBuilder.ToString();
+                                StringBuilder stringBuilder = new StringBuilder();
+                                foreach( byte b in hash )
+                                {
+                                    stringBuilder.AppendFormat( "{0:x2}", b );
+                                }
+                                string hashPass = stringBuilder.ToString();
 
-								txtBruger.Text = hashPass;
-
-
-
-								}
+                                txtBruger.Text = hashPass;
 
 
-								// if hashpass = dbpass{session["Authentication"] = crc32(user + rettighed);}
-								// else {string msg = "Brugernavn / adgangskode er ikke korrekt";}
 
-							}
-							else
-							{
-								txtBruger.Text = "Brugernavn / adgangskode er ikke korrekt";
-							}
-
-						}
-					}
+                            }
 
 
-				else
-				{
-					txtBruger.Text = "";
-					txtBruger.Visible = true;
-					txtAdgang.Visible = true;
-					btnLogin.Text = "Log ind";
-					session.Abandon();
-				}
-			}
-			catch( Exception e ) 
-			{
+                            // if hashpass = dbpass{session["Authentication"] = crc32(user + rettighed);}
+                            // else {string msg = "Brugernavn / adgangskode er ikke korrekt";}
 
-			}
-		} 
-	}
+                        }
+                        else
+                        {
+                            txtBruger.Text = "Brugernavn / adgangskode er ikke korrekt";
+                        }
+
+                    }
+                }
+                else
+                {
+                    txtBruger.Text = "";
+                    txtBruger.Visible = true;
+                    txtAdgang.Visible = true;
+                    btnLogin.Text = "Log ind";
+                    session.Abandon();
+                }
+            }
+            catch( Exception e )
+            {
+
+            }
+        }
+    }
 }
