da38b529676b5f2aadbcbdf901e6e40faf7cc542
Daniel V. Sandholt <gunsnap@gmail.com>
2014-11-12 11:31:34 +0100

    GaaTil erstatter GoTo funktionerne

--- FDFk7/FDFk7/Default.aspx.cs
+++ FDFk7/FDFk7/Default.aspx.cs
@@ -1,37 +1,34 @@
 using System;
 
-namespace FDFk7 {
-	public partial class Default : System.Web.UI.Page {
+namespace FDFk7
+{
+    public partial class Default : System.Web.UI.Page
+    {
 
-		protected void Page_Load( object sender, EventArgs args ) {
-			if( Session[ "UserAuthentication" ] != null ) {
-				Basis bob = new Basis( "Load", new object[]{ txtBruger, txtAdgang, btnLogin, Session, Request } );
-			}
-		}
+        protected void Page_Load( object sender, EventArgs args )
+        {//FIXME skal laves så der også Loades når man ikke er logget ind
+            if( Session["UserAuthentication"] != null )
+            {
+                Basis bob = new Basis( "Load", new object[]{ txtBruger, txtAdgang, btnLogin, Session, Request } );
+            }
+        }
 
-		public void LoginOut( object sender, EventArgs args ) {
-			if( btnLogin.Text.Substring( 4, 2 ) == "ud" ) {
-				Basis bob = new Basis( "Logout", new object[]{ txtBruger, txtAdgang, btnLogin, Session } );
-			} else if( txtBruger.Text != "" && txtAdgang.Text != "" ) {
-				Basis bob = new Basis( "Login", new object[]{ txtBruger, txtAdgang, btnLogin, Session } );
-			}
-		}
+        public void LoginOut( object sender, EventArgs args )
+        {//FIXME skal laves så den kan bruges i GaaTil
+            if( btnLogin.Text.Substring( 4, 2 ) == "ud" )
+            {
+                Basis bob = new Basis( "Logout", new object[]{ txtBruger, txtAdgang, btnLogin, Session } );
+            }
+            else if( txtBruger.Text != "" && txtAdgang.Text != "" )
+            {
+                Basis bob = new Basis( "Login", new object[]{ txtBruger, txtAdgang, btnLogin, Session } );
+            }
+        }
 
-		public void GotoForside( object sender, EventArgs args ) {
-			Basis bob = new Basis( "Forside", new object[]{ sender, Response, Session } );
-		}
+        public void GaaTil( object sender, EventArgs args )
+        {
+            Basis bob = new Basis( new object[]{ sender, Response, Session } );
+        }
 
-		public void GotoOmOs( object sender, EventArgs args ) {
-			Basis bob = new Basis( "OmOs", new object[]{ sender, Response, Session } );
-		}
-
-		public void GotoUdlejning( object sender, EventArgs args ) {
-			Basis bob = new Basis( "Udlejning", new object[]{ sender, Response, Session, Request } );
-		}
-
-		public void GotoKontakt( object sender, EventArgs args ) {
-			Basis bob = new Basis( "Kontakt", new object[]{ sender, Response, Session } );
-		}
-
-	}
+    }
 }
