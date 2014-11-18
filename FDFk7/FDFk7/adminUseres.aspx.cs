ae59fb778194e13b0deb64999bea396c3b5d7f49
Daniel V. Sandholt <gunsnap@gmail.com>
2014-11-18 13:32:12 +0100

    try om noget

--- FDFk7/FDFk7/adminUseres.aspx.cs
+++ FDFk7/FDFk7/adminUseres.aspx.cs
@@ -49,60 +49,66 @@ namespace FDFk7
 
             solutions.Rows[0].Cells.AddRange( tcList );
 
-            /*Indset data*/
-            // Opretter forbindelse til databasen
-            SqlConnection Con = new SqlConnection( "Data Source=mssql3.unoeuro.com;Initial Catalog=fdfk7_dk_db;Persist Security Info=True;User ID=fdfk7_dk;Password=4Xbc8tun" );
-            // Trækker data fra databasen
-            SqlCommand cmd_select = new SqlCommand( "SELECT USR_Ledere.Navn, USR_Gruppe.GruppeNavn, USR_Ledere.Telefon, USR_Brugere.MailAdresse, USR_Brugere.Rettighedder FROM USR_Gruppe, USR_Ledere, USR_Brugere WHERE USR_Ledere.LederID = USR_Brugere.FKBrugereLedereID AND USR_Ledere.LederID  = USR_Gruppe.Gruppe_Leder" );
-            //opretter forbindelse
-            cmd_select.Connection = Con;
-            Con.Open();
-            SqlDataReader DB_Reader = cmd_select.ExecuteReader();
-
-            int antal = 0;
-            while( DB_Reader.Read() )
+            try
             {
-                TableRow newRow = new TableRow();
+                /*Indset data*/
+                // Opretter forbindelse til databasen
+                SqlConnection Con = new SqlConnection( "Data Source=mssql3.unoeuro.com;Initial Catalog=fdfk7_dk_db;Persist Security Info=True;User ID=fdfk7_dk;Password=4Xbc8tun" );
+                // Trækker data fra databasen
+                SqlCommand cmd_select = new SqlCommand( "SELECT USR_Ledere.Navn, USR_Gruppe.GruppeNavn, USR_Ledere.Telefon, USR_Brugere.MailAdresse, USR_Brugere.Rettighedder FROM USR_Gruppe, USR_Ledere, USR_Brugere WHERE USR_Ledere.LederID = USR_Brugere.FKBrugereLedereID AND USR_Ledere.LederID  = USR_Gruppe.Gruppe_Leder" );
+                //opretter forbindelse
+                cmd_select.Connection = Con;
+                Con.Open();
+                SqlDataReader DB_Reader = cmd_select.ExecuteReader();
 
-                TableCell newFirstCell = new TableCell();
-                newFirstCell.Text = antal.ToString();
-                newRow.Cells.Add( newFirstCell );
-
-                int i = 0;
-                while( i < DB_Reader.FieldCount )
+                int antal = 0;
+                while( DB_Reader.Read() )
                 {
-                    TableCell newcell = new TableCell();
-                    newcell.Text = DB_Reader[i].ToString();
-                    newcell.ID = "Row" + antal + "Cell" + i;
-                    newRow.Cells.Add( newcell );
-                    i++;
+                    TableRow newRow = new TableRow();
+
+                    TableCell newFirstCell = new TableCell();
+                    newFirstCell.Text = antal.ToString();
+                    newRow.Cells.Add( newFirstCell );
+
+                    int i = 0;
+                    while( i < DB_Reader.FieldCount )
+                    {
+                        TableCell newcell = new TableCell();
+                        newcell.Text = DB_Reader[i].ToString();
+                        newcell.ID = "Row" + antal + "Cell" + i;
+                        newRow.Cells.Add( newcell );
+                        i++;
+                    }
+
+                    //Rediger knap
+                    Button Rediger = new Button();
+                    Rediger.Text = "Rediger";
+                    Rediger.ID = "btnR" + antal + "Rediger";
+                    Rediger.Click += RedigerBruger;
+                    newRow.Cells.Add( new TableCell() );
+                    newRow.Cells[i + 1].Controls.Add( Rediger );
+
+                    //Slet knap
+                    Button Slet = new Button();
+                    Slet.Text = "Slet";
+                    Slet.ID = "btnS" + antal + "Slet";
+                    Slet.Click += SletBruger;
+                    newRow.Cells.Add( new TableCell() );
+                    newRow.Cells[i + 2].Controls.Add( Slet );
+
+                    solutions.Rows.Add( newRow );
+
+                    antal++;
                 }
-
-                //Rediger knap
-                Button Rediger = new Button();
-                Rediger.Text = "Rediger";
-                Rediger.ID = "btnR" + antal + "Rediger";
-                Rediger.Click += RedigerBruger;
-                newRow.Cells.Add( new TableCell() );
-                newRow.Cells[i + 1].Controls.Add( Rediger );
-
-                //Slet knap
-                Button Slet = new Button();
-                Slet.Text = "Slet";
-                Slet.ID = "btnS" + antal + "Slet";
-                Slet.Click += SletBruger;
-                newRow.Cells.Add( new TableCell() );
-                newRow.Cells[i + 2].Controls.Add( Slet );
-
-                solutions.Rows.Add( newRow );
-
-                antal++;
+            } catch( Exception ex )
+            {
             }
         }
 
         public void RedigerBruger( object sender, EventArgs args )
         {
-            string tmpID = ((Button)sender).ID;
+//            string tmpID = ((Button)sender).ID;
+
         }
 
         public void SletBruger( object sender, EventArgs args )
